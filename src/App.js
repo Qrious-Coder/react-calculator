e8a325816cd88c5bf3ed16c8fadfcaed5da2f9a4
Qrious-Coder <q81188118@gmail.com>
2022-05-04 23:56:00 +0800

    route change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from './Components/Outlay/Navbar';
 import StatePage from './Components/Outlay/StatePage';
 import ReducerPage from './Components/Outlay/ReducerPage';
 import Footer from './Components/Outlay/Footer';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Routes} from 'react-router-dom';
 import { bgStyle } from './Components/styles/Styles'
 
 function App() {
@@ -14,10 +14,12 @@ function App() {
         <Navbar />
         <div className="content flex justify-center items-center">
           <Routes>
-            <Route path="/react-calculator" element={ <StatePage/> }>
-            </Route> 
-            <Route path="/react-calculator/ReducerPage" element={ <ReducerPage/>}>
-            </Route>
+            <switch>
+              <Route path="/react-calculator" exact component={ <StatePage/> }>
+              </Route> 
+              <Route path="/react-calculator/ReducerPage" exact component={ <ReducerPage/>}>
+              </Route>
+            </switch>
           </Routes>
         </div> 
         <Footer />   
